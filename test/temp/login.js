09b1a1e944e5563ac4c644c05ffba5ae7acdab9e
ignamiguel <ignacio.iglesias@mulesoft.com>
2017-10-13 23:30:08 -0300

    removed comments

--- test/temp/login.js
+++ test/temp/login.js
@@ -1,15 +1,5 @@
 const timeout = 10 * 1000;
 
-// describe('DEMO Protractor', () => {
-//   it('should navigate to login page at "the-internet"', () => {
-//     browser.get('http://localhost:9292/login');
-//     browser.sleep(timeout);
-    
-//     expect().toBe();
-//   });
-// });
-
-
 describe('Protractor DEMO', () => {
   it('should naviage the login page at "the-internet"', () => {
     browser.get('http://localhost:9292/login');
